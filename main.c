c164f430ee4b61d4c60844f6ea31f4ed70691fdc
SirDom247 <dominicudousoro@live.com>
2023-02-22 14:25:22 -0800

    Updated Simple Shell

--- main.c
+++ main.c
@@ -1,17 +1,47 @@
-#include "main.h"
+#include "shell.h"
+
 
 /**
- * main - Entry point for the simple shell program
- * @argc: ......
- * @argv: .....
- * @envp: .....
- * Return: 0 on successful completion of the program
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv, char **envp)
+int main(int ac, char **av)
 {
-	(void)argc;
-	(void)argv;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	shell_loop(envp);
-	return (0);
+
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
